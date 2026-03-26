acad385cad5e9db7a0310f6fd99af54842655429
Adam <rokakaak@email.com>
2026-03-26 22:34:49 +0200

    bd is added

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('My game')
 
-clock = pygame.time.Clock()
-
 running = True
+
 while running:
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill('purple')
+
+    # pygame.draw.rect(screen, "green", (100, 100, 200, 150))
+    # pygame.draw.rect(screen, "green", (980, 100, 200, 150))
+    # pygame.draw.rect(screen, "green", (550, 400, 200, 200))
+
+    pygame.display.flip()
 
 pygame.quit()
