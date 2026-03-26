7a4d7e8fd9aab7bcafa729fdd3f35a00d32078c4
Adam <rokakaak@email.com>
2026-03-26 22:18:20 +0200

    pygame basic structure

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('My game')
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
